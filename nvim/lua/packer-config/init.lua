11b2fd8ce7fdb2f7b3f47646f079acbac1a3cb3c
Mauricio De La Cruz <delacruzen@hotmail.com>
2022-10-19 21:33:17 -0300

    Update file

--- nvim/lua/packer-config/init.lua
+++ nvim/lua/packer-config/init.lua
@@ -5,7 +5,7 @@ return require'packer'.startup(function()
     use 'nvim-tree/nvim-tree.lua'
     use 'kyazdani42/nvim-web-devicons'
 
-    use 'neovim/nvim-lspconfig'
+    use 'neovim/nvim-lspconfig' -- LSP Plugin
     use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
     use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
     use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
