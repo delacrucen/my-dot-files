4c1f4bde5fa17fa4ed82b76d0478aae5f56c6db4
Mauricio De La Cruz <delacruzen@hotmail.com>
2022-10-29 16:13:52 -0300

    Change window management keys to not conflict with default leap trigger key

--- nvim/lua/mauri/core/keymaps.lua
+++ nvim/lua/mauri/core/keymaps.lua
@@ -26,10 +26,10 @@ keymap.set("n", "<leader>+", "<C-a>") -- increment
 keymap.set("n", "<leader>-", "<C-x>") -- decrement
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
-keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+keymap.set("n", "<leader>wv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>wh", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>we", "<C-w>=") -- make split windows equal width & height
+keymap.set("n", "<leader>wx", ":close<CR>") -- close current split window
 
 keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
